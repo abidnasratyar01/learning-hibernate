d2dd290120ec1854aa24b7b7bbeb5f24dfe8d2b2
Ahmad Abid <abidnasratyar01@gmail.com>
2023-02-26 11:16:18 +0430

    Learnt Hibernate basics set up and configs

--- hibernatelearning/src/main/java/com/hibernate/learning/App.java
+++ hibernatelearning/src/main/java/com/hibernate/learning/App.java
@@ -1,5 +1,10 @@
 package com.hibernate.learning;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        
+        Student std = new Student();
+        std.setId(01);
+        std.setName("Ahmad Abid");
+        
+        Session session = factory.openSession();
+        
+        Transaction tx = session.beginTransaction();
+        session.save(std);
+        tx.commit();
+               
+        session.close();
     }
 }
